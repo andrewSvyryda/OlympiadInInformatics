1ca3e62f547a418bf6eb5e74ea4293b090755974
AndriySvyryda <andrijsvirida2@gmail.com>
2021-10-22 14:19:55 +0300

    First task

---
+++ Task1/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Task1
+{
+    class Program
+    {
+        static int GetDigitsSum(int num)
+        {
+            return num == 0 ? 0 : num % 10 + GetDigitsSum(num / 10);
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine(GetDigitsSum(1344545345));
+        }
+    }
+}
