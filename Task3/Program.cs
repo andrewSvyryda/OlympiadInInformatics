110d745506a097e8cca150c06773881dcbc24ce5
AndriySvyryda <andrijsvirida2@gmail.com>
2021-10-22 14:38:11 +0300

    Task 3

--- Task3/Program.cs
+++ Task3/Program.cs
@@ -4,9 +4,22 @@ namespace Task3
 {
     class Program
     {
+        static double Sum(int x, int count)
+        {
+            double res = 0;
+
+            for (int i = 0; i < count * 2; i++)
+            {
+                int pow = i * 2 + 1;
+                Console.WriteLine(res);
+                res += Math.Pow(x, pow) / pow * (i % 2 == 0 ? 1 : -1);
+            }
+
+            return res;
+        }
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine(Sum(22, 10));
         }
     }
 }
