005222cce4cfc141bd0bf703eab96a11bbdcae8d
AndriySvyryda <andrijsvirida2@gmail.com>
2021-10-22 14:26:09 +0300

    Task2

--- Task2/Program.cs
+++ Task2/Program.cs
@@ -1,9 +1,16 @@
-﻿namespace Task2
+﻿using System;
+
+namespace Task2
 {
     class Program
     {
+        static double Func(double x, double y)
+        {
+            return Math.Pow(x, 3) / (3 * y) + Math.Sqrt(Math.Pow(x, 3) - 8 * x) + (3 * Math.Sin(y)) / Math.Cos(x / y);
+        }
         static void Main(string[] args)
         {
+            Console.WriteLine(Func(3, 3));
         }
     }
 }
